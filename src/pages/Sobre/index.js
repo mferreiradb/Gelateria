9b49969608e6de6337521a42962893ccbd0ea967
Mauricio Ferreira <99357847+mferreiradb@users.noreply.github.com>
2022-09-08 20:42:43 -0300

    Add files via upload

---
+++ src/pages/Sobre/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Header from '../../components/Header'
+
+export default function Sobre() {
+    return (
+        <div>
+            <Header />
+            <div>TESTE</div>
+        </div>
+    )
+}
